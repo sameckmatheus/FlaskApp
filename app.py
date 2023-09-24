c90478430c5f579d85879a6c44d2457b3ffa561f
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-23 21:55:11 -0300

    Update: Change Home Def to Play App

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
-
 app = Flask(__name__)
-@app.route('/')
 
+@app.route('/')
 def home():
-  return 'Flask em execução!...' 
+    return 'Flask em execução!...' 
+
+@app.route('/<nome>')
+def home_nome(nome):
+    return f'Hello, {nome}'
+
+if __name__=='_main_':
+    app.run(debug=True)
